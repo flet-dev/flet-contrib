65568c6eb80e1b54eb3457308ef0bcb43827eae0
InesaFitsner <inesa@appveyor.com>
2023-06-07 14:04:01 -0700

    renamed to right_pane and left_pane

--- flet_contrib/vertical_splitter/src/vertical_splitter.py
+++ flet_contrib/vertical_splitter/src/vertical_splitter.py
@@ -4,8 +4,8 @@ import flet as ft
 class VerticalSplitter(ft.Row):
     def __init__(
         self,
-        right_content,
-        left_content,
+        right_pane,
+        left_pane,
         height,
         spacing,
         min_width,
@@ -13,8 +13,8 @@ class VerticalSplitter(ft.Row):
         width=None,
     ):
         super().__init__(width=width, height=height, spacing=spacing)
-        self.right_content = right_content
-        self.left_content = left_content
+        self.right_pane = right_pane
+        self.left_pane = left_pane
         self.min_width = min_width
         self.max_width = max_width
         self.splitter = ft.GestureDetector(
@@ -24,17 +24,17 @@ class VerticalSplitter(ft.Row):
             on_hover=self.show_draggable_cursor,
         )
         self.controls = [
-            ft.Container(self.right_content),
+            ft.Container(self.right_pane),
             self.splitter,
-            self.left_content,
+            self.left_pane,
         ]
 
     def move_vertical_splitter(self, e: ft.DragUpdateEvent):
-        if (e.delta_x > 0 and self.right_content.width < self.max_width) or (
-            e.delta_x < 0 and self.right_content.width > self.min_width
+        if (e.delta_x > 0 and self.right_pane.width < self.max_width) or (
+            e.delta_x < 0 and self.right_pane.width > self.min_width
         ):
-            self.right_content.width += e.delta_x
-        self.right_content.update()
+            self.right_pane.width += e.delta_x
+        self.right_pane.update()
 
     def show_draggable_cursor(self, e: ft.HoverEvent):
         e.control.mouse_cursor = ft.MouseCursor.RESIZE_LEFT_RIGHT
@@ -63,8 +63,8 @@ def main(page: ft.Page):
     vertical_splitter = VerticalSplitter(
         height=400,
         spacing=0,
-        right_content=c_right,
-        left_content=c_left,
+        right_pane=c_right,
+        left_pane=c_left,
         min_width=50,
         max_width=300,
     )
