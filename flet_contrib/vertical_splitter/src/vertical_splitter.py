1afb12c65364212cb1c68e45055499db6df8b129
InesaFitsner <inesa@appveyor.com>
2023-06-07 14:29:12 -0700

    works for fixed left container

--- flet_contrib/vertical_splitter/src/vertical_splitter.py
+++ flet_contrib/vertical_splitter/src/vertical_splitter.py
@@ -8,16 +8,18 @@ class VerticalSplitter(ft.Row):
         left_pane,
         height,
         spacing,
-        min_width,
-        max_width,
+        fixed_pane_min_width=50,
+        fixed_pane_max_width=300,
+        fixed_pane_width=100,
         fixed_pane="left",
         width=None,
     ):
         super().__init__(width=width, height=height, spacing=spacing)
         self.right_pane = right_pane
         self.left_pane = left_pane
-        self.min_width = min_width
-        self.max_width = max_width
+        self.fixed_pane_min_width = fixed_pane_min_width
+        self.fixed_pane_max_width = fixed_pane_max_width
+        self.fixed_pane_width = fixed_pane_width
         self.fixed_pane = fixed_pane
         self.splitter = ft.GestureDetector(
             content=ft.VerticalDivider(),
@@ -25,18 +27,46 @@ class VerticalSplitter(ft.Row):
             on_pan_update=self.move_vertical_splitter,
             on_hover=self.show_draggable_cursor,
         )
+        # self.controls = [
+        #     ft.Container(self.left_pane),
+        #     self.splitter,
+        #     self.right_pane,
+        # ]
+        self.generate_layout()
+
+    def generate_layout(self):
+        if self.fixed_pane == "left":
+            self.left_container = ft.Container(
+                width=self.fixed_pane_width, content=self.left_pane
+            )
+            self.right_container = ft.Container(expand=1, content=self.right_pane)
+        elif self.fixed_pane == "right":
+            self.left_container = ft.Container(expand=1, content=self.left_pane)
+            self.right_container = ft.Container(
+                width=self.fixed_pane_width, content=self.right_pane
+            )
+
         self.controls = [
-            ft.Container(self.right_pane),
+            self.left_container,
             self.splitter,
-            self.left_pane,
+            self.right_container,
         ]
 
     def move_vertical_splitter(self, e: ft.DragUpdateEvent):
-        if (e.delta_x > 0 and self.right_pane.width < self.max_width) or (
-            e.delta_x < 0 and self.right_pane.width > self.min_width
-        ):
-            self.right_pane.width += e.delta_x
-        self.right_pane.update()
+        if self.fixed_pane == "right":
+            if (
+                e.delta_x > 0 and self.right_pane.width > self.fixed_pane_min_width
+            ) or (e.delta_x < 0 and self.right_pane.width < self.fixed_pane_max_width):
+                self.right_pane.width += e.delta_x
+            self.right_pane.update()
+        elif self.fixed_pane == "left":
+            if (
+                e.delta_x > 0 and self.left_container.width < self.fixed_pane_max_width
+            ) or (
+                e.delta_x < 0 and self.left_container.width > self.fixed_pane_min_width
+            ):
+                self.left_container.width += e.delta_x
+            self.left_container.update()
 
     def show_draggable_cursor(self, e: ft.HoverEvent):
         e.control.mouse_cursor = ft.MouseCursor.RESIZE_LEFT_RIGHT
@@ -48,18 +78,18 @@ def main(page: ft.Page):
         e.control.mouse_cursor = ft.MouseCursor.RESIZE_LEFT_RIGHT
         e.control.update()
 
-    c_right = ft.Container(
+    c_left = ft.Container(
         bgcolor=ft.colors.ORANGE_300,
         alignment=ft.alignment.center,
-        width=100,
+        # width=100,
         # expand=1,
     )
 
-    c_left = ft.Container(
+    c_right = ft.Container(
         bgcolor=ft.colors.BROWN_400,
         alignment=ft.alignment.center,
         # width=100,
-        expand=1,
+        # expand=1,
     )
 
     vertical_splitter = VerticalSplitter(
@@ -68,8 +98,9 @@ def main(page: ft.Page):
         right_pane=c_right,
         left_pane=c_left,
         fixed_pane="left",
-        min_width=50,
-        max_width=300,
+        fixed_pane_min_width=50,
+        fixed_pane_max_width=300,
+        fixed_pane_width=100,
     )
 
     page.add(vertical_splitter)
